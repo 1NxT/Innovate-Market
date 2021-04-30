55016ccf7a99e73fe47d1ce925f77fd5b870aa5a
IthaloCesar <80697419+IthaloCesar@users.noreply.github.com>
2021-04-30 15:26:00 -0300

    Erro de escrita

--- __init__/Pages/Caixa.py
+++ __init__/Pages/Caixa.py
@@ -1,12 +1,12 @@
 from tkinter import *
-class produtos:
+class Caixa:
     def __init__(self):
-        self.telaprodutos = Toplevel()
+        self.telacaixa = Toplevel()
         self.geometry()
 
     def geometry(self):
-        self.telaprodutos.title("Produtos")
-        self.telaprodutos.geometry("1360x760")
-        self.telaprodutos.configure(bg="DodgerBlue")
-        self.telaprodutos.resizable(False, False)
-        self.telaprodutos.iconbitmap('Innovate-Market-main\__init__\Imagens\logo.ico')
+        self.telacaixa.title("Caixa")
+        self.telacaixa.geometry("1360x760")
+        self.telacaixa.configure(bg="DodgerBlue")
+        self.telacaixa.resizable(False, False)
+        self.telacaixa.iconbitmap('Innovate-Market-main\__init__\Imagens\logo.ico')
