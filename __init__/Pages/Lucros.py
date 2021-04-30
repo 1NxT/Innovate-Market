c3fe711ca078c5a69bb12f9c7442a584c3717311
IthaloCesar <80697419+IthaloCesar@users.noreply.github.com>
2021-04-30 15:38:06 -0300

    CRIEI A SUBJANELA LUCRO
    OKKKKK

---
+++ __init__/Pages/Lucros.py
@@ -0,0 +1,12 @@
+from tkinter import *
+class Lucros:
+    def __init__(self):
+        self.telalucro = Toplevel()
+        self.geometry()
+
+    def geometry(self):
+        self.telalucro.title("Lucro")
+        self.telalucro.geometry("1360x760")
+        self.telalucro.configure(bg="DodgerBlue")
+        self.telalucro.resizable(False, False)
+        self.telalucro.iconbitmap('Innovate-Market-main\__init__\Imagens\logo.ico')
