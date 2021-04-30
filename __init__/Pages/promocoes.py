59ceb5648807009bb11d9e809a81673d07ca7aa8
IthaloCesar <80697419+IthaloCesar@users.noreply.github.com>
2021-04-30 15:07:17 -0300

    CRIEI A SUBJANELA PROMOCOES
    Subjanela  promocoes

---
+++ __init__/Pages/promocoes.py
@@ -0,0 +1,14 @@
+from tkinter import *
+class Promocoestela:
+    def __init__(self):
+        self.telap = Toplevel()
+        self.geometry()
+
+    def geometry(self):
+        self.telap.title = ("Promoçoes")
+        self.telap.geometry = ("1360x760")
+        self.telap.configure = (bg="DodgerBlue")
+        self.telap.resizable = (False, False)
+        self.telap,iconbitmap = ('Innovate-Market-main\__init__\Imagens\logo.ico')
+
+    
