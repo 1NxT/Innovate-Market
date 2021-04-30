d7e3e1091cf66ebedcad6f2285320ca44ee9a7cd
IthaloCesar <80697419+IthaloCesar@users.noreply.github.com>
2021-04-30 15:33:03 -0300

    CRIEI A SUBJANELA FORNECEDORES
    OKKKKKKKKKKKKKK

---
+++ __init__/Pages/Fornecedores.py
@@ -0,0 +1,12 @@
+from tkinter import *
+class Fornecedor:
+    def __init__(self):
+        self.telaforne = Toplevel()
+        self.geometry()
+
+    def geometry(self):
+        self.telaforne.title("Fornecedor")
+        self.telaforne.geometry("1360x760")
+        self.telaforne.configure(bg="DodgerBlue")
+        self.telaforne.resizable(False, False)
+        self.telaforne.iconbitmap('Innovate-Market-main\__init__\Imagens\logo.ico')
