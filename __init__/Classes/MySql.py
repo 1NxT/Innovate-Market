ea184c0a466a6ef818fa674cf516c25f726fc02e
Jair Francisco <82468545+JairFsl@users.noreply.github.com>
2021-05-01 16:32:18 -0300

    Corrigido local armazenado

--- __init__/Classes/MySql.py
+++ __init__/Classes/MySql.py
@@ -1,7 +1,7 @@
 import sqlite3
 class MySql: 
     def conectar(self):
-        banco = sqlite3.connect('Innovate-Market-main\__init__\data.db')
+        banco = sqlite3.connect('__init__\data.db')
         cursor = banco.cursor()
         self.__cursor = cursor
         return self.__cursor
