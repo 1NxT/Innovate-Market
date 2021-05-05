3f397d2d9e220183700b98f02debc294e49ce8dc
Jair Francisco <82468545+JairFsl@users.noreply.github.com>
2021-05-05 13:50:52 -0300

    Update Self

--- __init__/Pages/SegundaTela.py
+++ __init__/Pages/SegundaTela.py
@@ -35,20 +35,20 @@ class SegundaTela(Frame):
 
     def elementos(self):
         #Texto botões
-        btn_caixa = Button(self.tela2, text="Caixa", width=15,height=2, command=Caixa)
-        btn_caixa.grid(row=2, column=8)
+        self.btn_caixa = Button(self.tela2, text="Caixa", width=15,height=2, command=Caixa)
+        self.btn_caixa.grid(row=2, column=8)
 
-        btn_produtos = Button(self.tela2, text='Produtos',width=15, height=2, command=Produtos)
-        btn_produtos.grid(row=2, column=2)
+        self.btn_produtos = Button(self.tela2, text='Produtos',width=15, height=2, command=Produtos)
+        self.btn_produtos.grid(row=2, column=2)
 
-        btn_pedidos = Button(self.tela2, text='Pedidos', width=15,height=2, command=Pedidos)
-        btn_pedidos.grid(row=2, column=3)
+        self.btn_pedidos = Button(self.tela2, text='Pedidos', width=15,height=2, command=Pedidos)
+        self.btn_pedidos.grid(row=2, column=3)
 
-        btn_fornecedores = Button(self.tela2, text='Fornecedores', width=15, height=2, command=Fornecedor)
-        btn_fornecedores.grid(row=2, column=4)
+        self.btn_fornecedores = Button(self.tela2, text='Fornecedores', width=15, height=2, command=Fornecedor)
+        self.btn_fornecedores.grid(row=2, column=4)
 
-        btn_lucro = Button(self.tela2, text='Lucro', width=15, height=2, command=Lucros)
-        btn_lucro.grid(row=2, column=5)
+        self.btn_lucro = Button(self.tela2, text='Lucro', width=15, height=2, command=Lucros)
+        self.btn_lucro.grid(row=2, column=5)
 
         # btn_Editarpro = Button(self.tela2, text='Editar Produtos', width=15, height=2, command=editar_pro)
         # btn_Editarpro.grid(row=4, column=2)
@@ -65,34 +65,34 @@ class SegundaTela(Frame):
         # btn_editar_lucro = Button(self.tela2, text="Editar Lucro", width=15, height=2)
         # btn_editar_lucro.grid(row=4, column=5)
 
-        btn_promocao = Button(self.tela2, text='Promocoes',width=15, height=2, command=Promocoes)
-        btn_promocao.grid(row=2, column=6)
+        self.btn_promocao = Button(self.tela2, text='Promocoes',width=15, height=2, command=Promocoes)
+        self.btn_promocao.grid(row=2, column=6)
 
-        btn_gerenciar = Button(self.tela2, text='Gerenciar Usuários',width=15, height=2, command=Gerenciar)
-        btn_gerenciar.grid(row=2, column=7)
+        self.btn_gerenciar = Button(self.tela2, text='Gerenciar Usuários',width=15, height=2, command=Gerenciar)
+        self.btn_gerenciar.grid(row=2, column=7)
 
-        btn_voltar = Button(self.tela2, text="Logout", width=20,command=self.iExit2, bg="firebrick")
-        btn_voltar.place(x=1150, y=700)
+        self.btn_voltar = Button(self.tela2, text="Logout", width=20,command=self.iExit2, bg="firebrick")
+        self.btn_voltar.place(x=1150, y=700)
 
         #Imagens Botões
 
-        btn_img9 = Button(self.tela2, image=self.imgbtn9, width=140, height=140,bg="DodgerBlue", command=Caixa, relief="flat")
-        btn_img9.grid(row=1, column=8)
+        self.btn_img9 = Button(self.tela2, image=self.imgbtn9, width=140, height=140,bg="DodgerBlue", command=Caixa, relief="flat")
+        self.btn_img9.grid(row=1, column=8)
 
-        btn_img2 = Button(self.tela2, image=self.imgbtn2,  width=140, height=140,bg="DodgerBlue", command=Produtos, relief="flat")
-        btn_img2.grid(row=1, column=2)
+        self.btn_img2 = Button(self.tela2, image=self.imgbtn2,  width=140, height=140,bg="DodgerBlue", command=Produtos, relief="flat")
+        self.btn_img2.grid(row=1, column=2)
 
-        btn_img3 = Button(self.tela2, image=self.imgbtn3,  width=140, height=140,bg="DodgerBlue", command=Pedidos, relief="flat")
-        btn_img3.grid(row=1, column=3)
+        self.btn_img3 = Button(self.tela2, image=self.imgbtn3,  width=140, height=140,bg="DodgerBlue", command=Pedidos, relief="flat")
+        self.btn_img3.grid(row=1, column=3)
 
-        btn_img4 = Button(self.tela2, image=self.imgbtn4,  width=140, height=140,bg="DodgerBlue", command=Fornecedor, relief="flat")
-        btn_img4.grid(row=1, column=4)
+        self.btn_img4 = Button(self.tela2, image=self.imgbtn4,  width=140, height=140,bg="DodgerBlue", command=Fornecedor, relief="flat")
+        self.btn_img4.grid(row=1, column=4)
 
-        btn_img5 = Button(self.tela2, image=self.imgbtn5,  width=140, height=140,bg="DodgerBlue", command=Lucros, relief="flat")
-        btn_img5.grid(row=1, column=5)
+        self.btn_img5 = Button(self.tela2, image=self.imgbtn5,  width=140, height=140,bg="DodgerBlue", command=Lucros, relief="flat")
+        self.btn_img5.grid(row=1, column=5)
 
-        btn_img6 = Button(self.tela2, image=self.imgbtn6,  width=140,height=140, bg="DodgerBlue", command=Promocoes, relief="flat")
-        btn_img6.grid(row=1, column=6)
+        self.btn_img6 = Button(self.tela2, image=self.imgbtn6,  width=140,height=140, bg="DodgerBlue", command=Promocoes, relief="flat")
+        self.btn_img6.grid(row=1, column=6)
 
-        btn_img7 = Button(self.tela2, image=self.imgbtn7,  width=140, height=140,bg="DodgerBlue", command=Gerenciar, relief="flat")
-        btn_img7.grid(row=1, column=7)
+        self.btn_img7 = Button(self.tela2, image=self.imgbtn7,  width=140, height=140,bg="DodgerBlue", command=Gerenciar, relief="flat")
+        self.btn_img7.grid(row=1, column=7)
