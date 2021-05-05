156b0dc1161013ee9d36254dded3562f24b17145
Guilherme Rodrigues Oliveira <62958414+1NxT@users.noreply.github.com>
2021-05-05 15:41:10 -0300

    Classe 100% atualizada

--- __init__/Classes/Pesquisar.py
+++ __init__/Classes/Pesquisar.py
@@ -8,8 +8,12 @@ class Pesquisar():
         self.cursor = cursor
         
     def pesquisar(self, pesquisa, tabela, coluna):
+        keys = list(pesquisa)
+        if tabela == "fornecedor":
+            self.cursor.execute(f"SELECT * FROM {tabela} WHERE {keys[0]} LIKE '%{pesquisa[keys[0]]}%' OR {keys[1]} LIKE '%{pesquisa[keys[1]]}%' OR {keys[2]} LIKE '%{pesquisa[keys[2]]}%' OR {keys[3]} LIKE '%{pesquisa[keys[3]]}%' OR {keys[4]} LIKE '%{pesquisa[keys[4]]}%' ORDER BY {coluna}")
+        else:
+            self.cursor.execute(f"SELECT * FROM {tabela} WHERE {keys[0]} LIKE '%{pesquisa[keys[0]]}%' OR {keys[1]} LIKE '%{pesquisa[keys[1]]}%' OR {keys[2]} LIKE '%{pesquisa[keys[2]]}%' OR {keys[3]} LIKE '%{pesquisa[keys[3]]}%' ORDER BY {coluna}")  
         
-        self.cursor.execute(f"SELECT * FROM {tabela} WHERE nome_produto LIKE '%{str(pesquisa)}%' OR fornecedor LIKE '%{str(pesquisa)}%' OR preco LIKE '%{str(pesquisa)}%' OR id_produto LIKE '%{str(pesquisa)}%' ORDER BY {coluna}")  
         resultado = self.cursor.fetchall()     
         return resultado  
         
