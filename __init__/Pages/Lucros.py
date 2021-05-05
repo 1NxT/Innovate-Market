d4cbd8890191d7e7f8fa6a4415ad88cb4f5fdfcb
Jair Francisco <82468545+JairFsl@users.noreply.github.com>
2021-05-05 13:35:14 -0300

    Adicionado os elementos

--- __init__/Pages/Lucros.py
+++ __init__/Pages/Lucros.py
@@ -1,8 +1,13 @@
 from tkinter import *
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 class Lucros:
     def __init__(self):
         self.telalucro = Toplevel()
         self.geometry()
+        
+        self.elementos()
 
     def geometry(self):
         self.telalucro.title("Lucro")
@@ -10,3 +15,17 @@ class Lucros:
         self.telalucro.configure(bg="DodgerBlue")
         self.telalucro.resizable(False, False)
         self.telalucro.iconbitmap('__init__\Imagens\logo.ico')
+        
+    def voltar_inicial_lucros(self):
+        self.telalucro.destroy()
+        return
+    
+    def elementos(self):        
+        self.btn_telainicial_lucro = Button(self.telalucro, text="Voltar Para Tela Inicial", command=self.voltar_inicial_lucros, bg="red")
+        self.btn_telainicial_lucro.place(x=430, y=550)
+        
+        self.lbl_lucro= Label(self.telalucro, text="Lucros:", font="arial 18", bg="DodgerBlue", width=13)
+        self.lbl_lucro.place(x=1,y=1 )
+        
+        self.sns.lineplot(data=variavel_dos_dados)
+        #self.plt.show()
