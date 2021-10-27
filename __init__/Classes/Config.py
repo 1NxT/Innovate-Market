6102e911056f4673300e2376f548e60af97b6f76
Guilherme Rodrigues Oliveira <guirodrigues7863@gmail.com>
2021-10-27 10:14:09 -0300

    Update Config.py

--- __init__/Classes/Config.py
+++ __init__/Classes/Config.py
@@ -2,11 +2,11 @@ from pathlib import Path
 
 class Config():
     def  __init__(self):
-        self.databasepath = Path("__init__/data3.db")
-        self.imagespath = Path("__init__/Img")
+        self.__databasepath = Path("__init__/data3.db")
+        self.__imagespath = Path("__init__/Img")
         
     def database(self):
-        return self.databasepath
+        return self.__databasepath
     
     def images(self):
-        return self.imagespath
+        return self.__imagespath
