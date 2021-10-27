24e68bae2511e83a652102fccc1f32960df6fce7
Guilherme Rodrigues Oliveira <guirodrigues7863@gmail.com>
2021-10-27 11:16:10 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:/Users/ProjetoFRB/AppData/Local/Programs/Python/Python39/python.exe" ":/Users/ProjetoFRB/Documents/GitHub/Innovate-Market/InnovateMarket/main.py"
+"C:/Users/ProjetoFRB/AppData/Local/Programs/Python/Python39/python.exe" "C:/Users/ProjetoFRB/Documents/GitHub/Innovate-Market/InnovateMarket/main.py"
 pause
