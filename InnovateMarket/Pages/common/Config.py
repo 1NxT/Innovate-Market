33617c06eb3218f6673702ad87cad29a20c327a0
Guilherme Rodrigues Oliveira <62958414+1NxT@users.noreply.github.com>
2021-11-02 19:41:51 -0300

    Update Config.py

--- InnovateMarket/Pages/common/Config.py
+++ InnovateMarket/Pages/common/Config.py
@@ -1,4 +1,8 @@
 from pathlib import Path
 
 databasepath = Path("./InnovateMarket/data.db")
+<<<<<<< Updated upstream
 imagespath = Path("./InnovateMarket/Img")
+=======
+imagespath = Path("./InnovateMarket/Img")
+>>>>>>> Stashed changes
