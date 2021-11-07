73e9e31f9a96162d70e84813e753fe1ed3ed6c1e
Ithalo Cesar <80697419+IthaloCesar@users.noreply.github.com>
2021-11-07 17:47:18 -0300

    Redimencionamento e entry

--- InnovateMarketMVC/view/Caixa.py
+++ InnovateMarketMVC/view/Caixa.py
@@ -1,7 +1,8 @@
 from tkinter import *
 import tkinter.ttk as ttk
-from controller.Controller import *
-from model.Config import *
+from Classes.Pesquisar import *
+from Classes.Mostrar import *
+from Pages.common.Config import *
 
 
 class Caixa(Frame):
@@ -33,7 +34,7 @@ class Caixa(Frame):
     
     # Função para aparecer os dados na Treeview
     def view_tree(self):
-        resultado = caixaControler().mostarCaixa()
+        resultado = Mostrar().mostrar(self, "caixa", "ID")
         
         if resultado != None:
             self.tree_caixa.delete(*self.tree_caixa.get_children())
@@ -45,7 +46,7 @@ class Caixa(Frame):
     
     # Função para procurar por dados na Treeview        
     def chamaPesquisar(self):
-        resultado = caixaControler().pesquisarPromocoes(self.ent_pesquisar.get())
+        resultado = Pesquisar.pesquisar(self.ent_pesquisar.get(), "caixa")
 
         if resultado != None:
             self.tree_caixa.delete(*self.tree_caixa.get_children())
@@ -58,6 +59,13 @@ class Caixa(Frame):
     
     # LABELS, ENTRYS, BUTTONS e ETC da tela:
     def elementos(self):
+
+
+        self.pathBg = imagespath / "caixa_bg.png"
+        self.__bg = PhotoImage(file =self.pathBg)
+        self.lblimgbg = Label(self.telacaixa, image=self.__bg)
+        self.lblimgbg.place(x=0, y=0)
+
         #Button de voltar a tela inicial
         self.btn_telainicial_caixa = Button(self.telacaixa, text="Voltar Para Tela Inicial", command=self.voltar_inicial_caixa,bg="firebrick")
         self.btn_telainicial_caixa.place(x=1200, y=700)
@@ -67,8 +75,8 @@ class Caixa(Frame):
         self.style.theme_use("default")
 
         # Frame da Treeview Caixa
-        self.tree_caixa_frame = Frame(self.telacaixa, padx=1, pady=3, bg="DodgerBlue")
-        self.tree_caixa_frame.place(x=0, y=0)
+        self.tree_caixa_frame = Frame(self.telacaixa, padx=1, pady=3, bg="lightgrey")
+        self.tree_caixa_frame.place(x=530, y=0)
 
         # ScrollBar Caixa
         self.scroll = ttk.Scrollbar(self.tree_caixa_frame)
@@ -82,53 +90,24 @@ class Caixa(Frame):
         self.scroll.config(command=self.tree_caixa.yview)
         
         # Colunas da Treeview Caixa
-        self.tree_caixa.heading('#1', text="Nome do Item", anchor=CENTER)
-        self.tree_caixa.heading('#2', text="Preço", anchor=CENTER)
-        self.tree_caixa.heading('#3', text="Fornecedor", anchor=CENTER)
-        self.tree_caixa.heading('#4', text="ID do Produto", anchor=CENTER)
+        self.tree_caixa.heading('#1', text="Codigo", anchor=CENTER)
+        self.tree_caixa.heading('#2', text="Descricao", anchor=CENTER)
+        self.tree_caixa.heading('#3', text="Qtde", anchor=CENTER)
+        self.tree_caixa.heading('#4', text="Total", anchor=CENTER)
         self.view_tree()
-
-        # Imagem da Logo
-        self.lbl_imagem_caixa = Label(self.telacaixa, image=self.imgcaixa, width=280, height=240, bg="DodgerBlue")
-        self.lbl_imagem_caixa.place(x=1100,y=0)
-
         # Menu de opções
-        self.menu_quantidade = OptionMenu(self.telacaixa, self.itemVariable, *self.options)
-        self.menu_quantidade.place(x=840,y=300)
+        #self.menu_quantidade = OptionMenu(self.telacaixa, self.itemVariable, *self.options)
+        #self.menu_quantidade.place(x=840,y=300)
 
         # Botões da tela Caixa
-        self.btn_editar = Button(self.telacaixa, text="Editar", font="arial 18")
-        self.btn_editar.place(x=1150,y=400)
-
-        self.btn_cancelar = Button(self.telacaixa, text="Cancelar", font="arial 18")
-        self.btn_cancelar.place(x=850,y=400)
-
-        self.btn_final = Button(self.telacaixa, text="Finalizar", font="arial 18")
-        self.btn_final.place(x=1150,y=400)
-
-        self.btn_excluir = Button(self.telacaixa, text="Excluir", font="arial 18")
-        self.btn_excluir.place(x=1000,y=400)
+        
 
         self.ent_cod_barras = Entry(self.telacaixa, font="arial 18")
-        self.ent_cod_barras.place(x=830, y=60)
-
-        
-        self.lbl_subtotal = Label(self.telacaixa,text="SUB TOTAL:", font="arial 24",fg="black",bg="DodgerBlue")
-        self.lbl_subtotal.place(x=0,y=527)
-
-        self.lbl_total_recebido = Label(self.telacaixa, text="Total Recebido:", font="arial 24",bg="DodgerBlue")
-        self.lbl_total_recebido.place(x=0,y=640)
-
-        self.lbl_total_unitario = Label(self.telacaixa, text="Valor Unitario :", font="arial 24", bg="DodgerBlue")
-        self.lbl_total_unitario.place(x=830,y=100)
+        self.ent_cod_barras.place(x=50, y=80)
 
 
-        self.lbl_troco = Label(self.telacaixa, text="Troco:", font="arial 24",bg="DodgerBlue")
-        self.lbl_troco.place(x=470,y=640)
 
-        self.lbl_codigo = Label(self.telacaixa, text="ID do Produto:", font="arial 24",bg="DodgerBlue")
-        self.lbl_codigo.place(x=830,y=10)
+        self.ent_qtde = Entry(self.telacaixa, font="arial 18")
+        self.ent_qtde.place(x=50, y=460)
 
-        self.lbl_total_item = Label(self.telacaixa, text="Total do item:", font="arial 24",bg="DodgerBlue")
-        self.lbl_total_item.place(x=830,y=200)
         
