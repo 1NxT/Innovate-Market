e22cf13cbe46784d3d32c188e4b7690b0d6c6446
Ithalo Cesar <80697419+IthaloCesar@users.noreply.github.com>
2021-11-07 17:32:21 -0300

    Combo box e alteração nas dimenções

--- InnovateMarketMVC/view/Gerenciar.py
+++ InnovateMarketMVC/view/Gerenciar.py
@@ -1,7 +1,8 @@
-from tkinter import *
+from tkinter import * 
 import tkinter.ttk as ttk
-from controller.Controller import gerenciarControler
-from model.Config import *
+from Classes.Pesquisar import *
+from Classes.Mostrar import *
+from Pages.common.Config import *
 
 class Gerenciar :
     def __init__(self):
@@ -15,16 +16,19 @@ class Gerenciar :
         self.telageren.geometry("1360x760")
         self.telageren.configure(bg="DodgerBlue")
         self.telageren.resizable(False, False)
+        self.options = ["Caixa", "Gerente"]
+        self.itemVariable = StringVar()
+        self.itemVariable.set(self.options[0])
         # self.__iconImagemPath = imagespath / "logo.ico"
         # self.telageren.iconbitmap(self.__iconImagemPath)
 
-    def tela_inicial_gerenciar(self):
-            cadastro_usuario.destroy()
-            return
+    # def tela_inicial_gerenciar(self):
+    #         cadastro_usuario.destroy()
+    #         return
     
     
     def view_tree(self):
-        resultado = gerenciarControler().mostarGerenciar()
+        resultado = Mostrar().mostrar(self, "user", "CPF")
         
         if resultado != None:
             self.tree_gere.delete(*self.tree_gere.get_children())
@@ -40,7 +44,7 @@ class Gerenciar :
 
     # Função para procurar por dados na Treeview        
     def chamaPesquisar(self):
-        resultado = gerenciarControler().pesquisarGerenciar(self.ent_pesquisar.get())
+        resultado = Pesquisar().pesquisar(self.ent_pesquisar.get(), "usuarios")
 
         if resultado != None:
             self.tree_gere.delete(*self.tree_gere.get_children())
@@ -52,52 +56,58 @@ class Gerenciar :
 
     def elementos(self):
 
-        self.btn_sair_gere = Button(self.telageren,text="Voltar para a tela inicial", font="arial 12",bg="red",command=self.tela_inicial_gerenciar)
-        self.btn_sair_gere.place(x=1100,y=700)
+
+        self.pathBg = imagespath / "gerenciar_bg.png"
+        self.__bg = PhotoImage(file =self.pathBg)
+        self.lblimgbg = Label(self.telageren, image=self.__bg)
+        self.lblimgbg.place(x=0, y=0)
+
+        self.btn_telainicial = imagespath / "voltar.png"
+        self.btn_voltartelainicial = PhotoImage(file =self.btn_telainicial)
+        self.btn_telainicial_pedi = Button(self.telageren, image=self.btn_voltartelainicial, command=self.tela_inicial_gerenciar, relief="flat", borderwidth=0, width=224, height=50, bg="Gainsboro")
+        self.btn_telainicial_pedi.place(x=980, y=660)
+
+       
 
         # Estilo da Treeview
         self.style = ttk.Style()
         self.style.theme_use("default")
 
         # Frame da Treeview
-        self.tree_gere_frame = Frame(self.telageren, padx=1, pady=5, bg="DodgerBlue")
+        self.tree_gere_frame = Frame(self.telageren, padx=1, pady=5, bg="lightgrey")
         self.tree_gere_frame.place(x=0, y=0)
 
         # ScrollBar
         self.scroll = ttk.Scrollbar(self.tree_gere_frame)
         self.scroll.pack(side=RIGHT, fill=Y, padx=0)
 
-        self.tree_gere = ttk.Treeview(self.tree_gere_frame, columns=("1","2"), show="headings",height=35, yscrollcommand=self.scroll.set)
+        self.tree_gere = ttk.Treeview(self.tree_gere_frame, columns=("1","2","3","4"), show="headings",height=28, yscrollcommand=self.scroll.set)
         self.tree_gere.pack()
 
         self.scroll.config(command=self.tree_gere.yview)
 
-        self.tree_gere.heading('#1',text="Nome De Usuario", anchor=CENTER)
+        self.tree_gere.heading('#1',text="Nome ", anchor=CENTER)
         self.tree_gere.heading('#2',text="Senha Do Usuario", anchor=CENTER)
+        self.tree_gere.heading('#3',text="CPF do Usuario", anchor=CENTER)
+        self.tree_gere.heading('#4',text="Cargo", anchor=CENTER)
         self.view_tree()
-        
 
-        self.lbl_nome = Label(self.telageren,text="Nome Do Usuario: ",font="arial 12")
-        self.lbl_nome.place(x=400,y=100,relwidth=0.17)
+        self.ent_pesquisar = Entry(self.telageren,bg="lightgrey", font= "arial 18", width= "25")
+        self.ent_pesquisar.place(x=900,y=70)
 
-        self.ent_nome = Entry(self.telageren,bg="lightgrey")
-        self.ent_nome.place(x=700,y=100,relwidth=0.24)
+        self.ent_nome = Entry(self.telageren,bg="lightgrey",font= "arial 18", width= "25")
+        self.ent_nome.place(x=900,y=205)
         
-        self.ent_pesquisar = Entry(self.telageren,bg="lightgrey")
-        self.ent_pesquisar.place(x=700,y=600)
+        self.ent_senha = Entry(self.telageren,bg="lightgrey",font= "arial 18", width= "25")
+        self.ent_senha.place(x=900,y=320)
+
+        self.ent_cpf = Entry(self.telageren,bg="lightgrey",font= "arial 18", width= "25")
+        self.ent_cpf.place(x=900,y=440)
+
+
+
+        self.menu_quantidade = OptionMenu(self.telageren, self.itemVariable, *self.options)
+        self.menu_quantidade.place(x=900,y=550)
     
-        self.lbl_senha= Label(self.telageren,text="Senha: ",font="arial 12")
-        self.lbl_senha.place(x=500,y=200,relwidth=0.07)
-
-        self.ent_senha = Entry(self.telageren,bg="lightgrey")
-        self.ent_senha.place(x=700,y=200,relwidth=0.24)
-
-        self.lbl_erro = Label(self.telageren,text="", font="arial 12 ", fg="green", bg="DodgerBlue")
-        self.lbl_erro.place(x=500,y=300,relwidth=0.17)
-        
-        self.btn_enviar = Button(self.telageren,text="Salvar Dados", font="arial 12")
-        self.btn_enviar.place(x=500,y=400,relwidth=0.24)
 
 
-        self.btn_excluir = Button(self.telageren,text="Excluir", font="arial 12")
-        self.btn_excluir.place(x=500,y=500,relwidth=0.24)
